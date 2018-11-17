81adffd0783f2b7801808bd4bd85873eaefee239
Bryan <btc3270@hotmail.com>
2018-11-16 20:18:45 -0500

    5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SessionsListComponent } from './sessions/sessions-list/sessions-list.co
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { async } from 'q';
 
 @NgModule({
   declarations: [
